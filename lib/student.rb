77995b4e2ebff6b250b0a74a974cdd0bd7fd5306
Sean Oughton <seanoughton@gmail.com>
2018-03-12 22:38:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
+      ORDER BY students.id
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
